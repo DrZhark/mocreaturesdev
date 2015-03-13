47573e13612a33065cd3369a30b9852637595f73
Dan Roque <jdroque@gmail.com>
2015-03-12 23:24:55 -0400

    Fix Werewolf rendering.

--- src/main/java/drzhark/mocreatures/client/renderer/entity/MoCRenderWerewolf.java
+++ src/main/java/drzhark/mocreatures/client/renderer/entity/MoCRenderWerewolf.java
@@ -7,8 +7,10 @@ import drzhark.mocreatures.client.model.MoCModelWereHuman;
 import drzhark.mocreatures.entity.monster.MoCEntityWerewolf;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -20,9 +22,7 @@ public class MoCRenderWerewolf extends RenderLiving {
 
     public MoCRenderWerewolf(MoCModelWereHuman modelwerehuman, ModelBase modelbase, float f) {
         super(MoCClientProxy.mc.getRenderManager(), modelbase, f);
-        // TODO
-        //setRenderPassModel(modelwerehuman);
-        //tempWerewolf = (MoCModelWerewolf) modelbase;
+        this.addLayer(new LayerMoCWereHuman(this));
         this.tempWerewolf = (MoCModelWere) modelbase;
     }
 
@@ -34,42 +34,62 @@ public class MoCRenderWerewolf extends RenderLiving {
 
     }
 
-    protected int shouldRenderPass(MoCEntityWerewolf entitywerewolf, int i) {
-        int myType = entitywerewolf.getType();
-
-        if (!entitywerewolf.getIsHumanForm()) {
-            this.bindTexture(MoCreatures.proxy.getTexture("wereblank.png"));
-        } else {
-            switch (myType) {
-
-                case 1:
-                    this.bindTexture(MoCreatures.proxy.getTexture("weredude.png"));
-                    break;
-                case 2:
-                    this.bindTexture(MoCreatures.proxy.getTexture("werehuman.png"));
-                    break;
-                case 3:
-                    this.bindTexture(MoCreatures.proxy.getTexture("wereoldie.png"));
-                    break;
-                case 4:
-                    this.bindTexture(MoCreatures.proxy.getTexture("werewoman.png"));
-                    break;
-                default:
-                    this.bindTexture(MoCreatures.proxy.getTexture("wereoldie.png"));
-            }
-
-        }
-        return 1;
-    }
-
-    /*
-    @Override
-    protected int shouldRenderPass(EntityLivingBase entityliving, int i, float f) {
-        return shouldRenderPass((MoCEntityWerewolf) entityliving, i);
-    }*/
-
     @Override
     protected ResourceLocation getEntityTexture(Entity par1Entity) {
         return ((MoCEntityWerewolf) par1Entity).getTexture();
+    }
+
+    private class LayerMoCWereHuman implements LayerRenderer {
+
+        private final MoCRenderWerewolf mocRenderer;
+        private final MoCModelWereHuman mocModel = new MoCModelWereHuman();
+
+        public LayerMoCWereHuman(MoCRenderWerewolf p_i46112_1_) {
+            this.mocRenderer = p_i46112_1_;
+        }
+
+        public void doRenderLayer(MoCEntityWerewolf p_177162_1_, float p_177162_2_, float p_177162_3_, float p_177162_4_, float p_177162_5_,
+                float p_177162_6_, float p_177162_7_, float p_177162_8_) {
+            int myType = p_177162_1_.getType();
+
+            if (!p_177162_1_.getIsHumanForm()) {
+                bindTexture(MoCreatures.proxy.getTexture("wereblank.png"));
+            } else {
+                switch (myType) {
+
+                    case 1:
+                        bindTexture(MoCreatures.proxy.getTexture("weredude.png"));
+                        break;
+                    case 2:
+                        bindTexture(MoCreatures.proxy.getTexture("werehuman.png"));
+                        break;
+                    case 3:
+                        bindTexture(MoCreatures.proxy.getTexture("wereoldie.png"));
+                        break;
+                    case 4:
+                        bindTexture(MoCreatures.proxy.getTexture("werewoman.png"));
+                        break;
+                    default:
+                        bindTexture(MoCreatures.proxy.getTexture("wereoldie.png"));
+                }
+
+            }
+
+            this.mocModel.setModelAttributes(this.mocRenderer.getMainModel());
+            this.mocModel.setLivingAnimations(p_177162_1_, p_177162_2_, p_177162_3_, p_177162_4_);
+            this.mocModel.render(p_177162_1_, p_177162_2_, p_177162_3_, p_177162_5_, p_177162_6_, p_177162_7_, p_177162_8_);
+        }
+
+        @Override
+        public boolean shouldCombineTextures() {
+            return true;
+        }
+
+        @Override
+        public void doRenderLayer(EntityLivingBase p_177141_1_, float p_177141_2_, float p_177141_3_, float p_177141_4_, float p_177141_5_,
+                float p_177141_6_, float p_177141_7_, float p_177141_8_) {
+            this.doRenderLayer((MoCEntityWerewolf) p_177141_1_, p_177141_2_, p_177141_3_, p_177141_4_, p_177141_5_, p_177141_6_, p_177141_7_,
+                    p_177141_8_);
+        }
     }
 }
